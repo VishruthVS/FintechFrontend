4c1e33466f353f7a2b0d0029aa94519447993bc8
Vishruth <vishruthvs2003@gmail.com>
2024-03-08 19:08:05 +0530

    ASDF

--- frontend/components/CryptoData.js
+++ frontend/components/CryptoData.js
@@ -69,7 +69,8 @@ const CryptoNews = () => {
   return (
     <div>
       <h1 className="text-2xl font-bold mb-4">Crypto News</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"> */}
+      <div className="grid grid-cols-1 gap-4">
         {Array.isArray(news) && news.length > 0 ? ( // Check if news is an array and has items
           news.map((item) => (
             <div key={item.event_id} className="border p-4 rounded-md">
--- news/src/app/page.tsx
+++ news/src/app/page.tsx
@@ -1,14 +1,14 @@
-import CryptoData from "@/components/CryptoData";
+// import CryptoData from "@/components/CryptoData";
 
-export default function Home() {
-  return (
-    // <main className="mt-4">
-    //   {/* <TopHeadlines /> */}
-    //   <CryptoData />
+// export default function Home() {
+//   return (
+//     // <main className="mt-4">
+//     //   {/* <TopHeadlines /> */}
+//     //   <CryptoData />
 
-    // </main>
-    <div className="container mx-auto px-4 py-8">
-      <CryptoData />
-    </div>
-  );
-}
+//     // </main>
+//     <div className="container mx-auto px-4 py-8">
+//       <CryptoData />
+//     </div>
+//   );
+// }
