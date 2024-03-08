88f54aa8e7a708b4fe2e142d2a5f7a456fc5bc53
SudeepGowda55 <ssudeepgowda55@gmail.com>
2024-03-08 17:20:50 +0530

    faucets linked

--- frontend/app/dev/faucets/page.js
+++ frontend/app/dev/faucets/page.js
@@ -1,5 +1,19 @@
+import Card from "@/components/Card";
+import faucets from "@/assets/chains.json"
+
 function Faucets() {
-  return <div>page</div>;
+  return (
+    <>
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-8">Testnet Faucets</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+          {faucets.map((chain, index) => (
+            <Card key={index} title={chain.title} description={chain.description} chain={chain.chain} url={chain.url} logo={chain.logo} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Faucets;
--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,19 +1,22 @@
-// components/Card.jsx
+"use client"
 
-const CardStyle = ({ logo, title, description, chain }) => {
+import Image from "next/image";
+import Link from "next/link";
+
+const Card = ({ logo, title, description, chain, url }) => {
   return (
     <div className="bg-white p-6 rounded-lg shadow-md">
       <div className="flex items-center mb-4">
-        {logo && <img src={logo} alt={title} className="h-8 w-8 mr-2" />}
-        <h3 className="text-lg font-semibold">{title}</h3>
+        {logo && <Image src={logo} alt={title} className="h-20 w-20 mr-2" width={100} height={100} />}
+        <p className="text-lg font-semibold">{title}</p>
       </div>
-      <p className="text-gray-600 mb-4">{description}</p>
-      {chain && <p className="text-sm text-gray-500">Chains: {chain}</p>}
-      <a href="#" className="text-blue-500 hover:text-blue-700 font-semibold">
-        Learn More
-      </a>
+      <div>
+        <p className="text-gray-600 mb-4">{description}</p>
+        {chain && <p className="text-sm text-gray-500">Chain: {chain}</p>}
+        <Link href={url} className="text-blue-500 hover:text-blue-700 font-semibold"><button>Go to Faucet</button></Link>
+      </div>
     </div>
   );
 };
 
-export default CardStyle;
+export default Card;
